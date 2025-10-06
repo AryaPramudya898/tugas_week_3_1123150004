e069bb23d80029cef0d2ebc580fc96e6da905b2b
AryaPramudya898 <aryapramudya610@gmail.com>
2025-10-06 11:20:24 +0700

    sung aja

---
+++ bin/tugas_week_3_1123150004.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week_3_1123150004/tugas_week_3_1123150004.dart' as tugas_week_3_1123150004;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week_3_1123150004.calculate()}!');
+}
---
+++ test/tugas_week_3_1123150004_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week_3_1123150004/tugas_week_3_1123150004.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
